519f57cc714611cb6fcb7526fe2ac993c5aa879a
Amayuru1999 <amarasingheau@gmail.com>
2024-08-28 13:19:11 +0530

    chore: Add newsku function to update inventory prices in euros

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as v2 from 'firebase-functions/v2';
+import * as v1 from 'firebase-functions/v1';
 
 type Indexable = { [key: string]: any };
 
@@ -8,3 +9,13 @@ export const helloWorld = v2.https.onRequest((req,res) => {
     const message = items[name] ;
     res.send(`<h1>${message}</h1>`);
 })
+
+type Sku = {name: string; usd: number; eur?: number};
+const USD_TO_EUROS = 0.85;
+
+export const newsku = v1.firestore.document('/inventory/{sku}')
+    .onCreate(snapshot => {
+        const data = snapshot.data() as Sku;
+        const eur =data.usd * USD_TO_EUROS;
+        return snapshot.ref.set({eur}, {merge: true});
+    });
