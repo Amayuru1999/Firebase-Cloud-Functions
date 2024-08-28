1c59fdd5d7d6cd7291059d6f663c6b73079b2bef
Amayuru1999 <amarasingheau@gmail.com>
2024-08-28 12:26:53 +0530

    refactor: Remove leading slash from name parameter in helloWorld function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as v2 from 'firebase-functions/v2';
 type Indexable = { [key: string]: any };
 
 export const helloWorld = v2.https.onRequest((req,res) => {
-    const name = req.params[0];
+    const name = req.params[0].replace('/','');
     const items: Indexable = {lamp:'This is a lamp',chair:'This is a chair'};
     const message = items[name] ;
     res.send(`<h1>${message}</h1>`);
